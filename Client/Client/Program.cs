53cb26c5c2f122334cc64445da761b0f5590d5fb
Sakshi Sharma <sakshis@tavisca.com>
2017-09-06 21:00:11 +0530

    Files added

---
+++ Client/Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Ram", "Kumar", "Sharma", 81342252575);
+            string data = JsonConvert.SerializeObject(person);
+            TcpClient client = new TcpClient();
+            client.Connect(IPAddress.Parse("127.0.0.1"),545);
+            byte[] dataBytes = Encoding.ASCII.GetBytes(data);
+            client.GetStream().Write(dataBytes, 0, dataBytes.Length);
+            Console.Read();
+        }
+    }
+}
---
+++ NewServer/NewServer/Server.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace NewServer
+{
+    class Server
+    {
+        TcpClient client;
+        TcpListener tcplistn; 
+        public void StartServer()
+        {
+            tcplistn =new TcpListener(IPAddress.Parse("127.0.0.1"), 545); 
+            tcplistn.Start();
+
+        }
+        public void AcceptClient()
+        {
+            client = tcplistn.AcceptTcpClient();
+           
+        }
+        public string GetData()
+        {
+            byte[] dataRead = new byte[10000];
+            client.GetStream().Read(dataRead, 0, dataRead.Length);
+            string recieved = Encoding.ASCII.GetString(dataRead);
+            return recieved;
+
+        }
+
+
+    }
+}
